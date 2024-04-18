4a2bec28903068631b7803e667ed6238e5bb7566
Vedant Dagadkhair <139427232+vedu111@users.noreply.github.com>
2024-04-18 22:23:57 +0530

    question.js

--- server/models/question.js
+++ server/models/question.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 
 const questionsubSchema = new mongoose.Schema({
+    sr_no: String, // Added sr_no field to schema
     type: String,
-    sr_no: String,
     questions: String,
     co: String,
     rbt: String,
     pi: String,
     marks: String,
-    subject: String // Added subject field to schema
-}); // Specify the collection name explicitly as 'questions'
+    subject: String, // Added subject field to schema
+    is_image: String
+});
 
 const Question = mongoose.model('Question', questionsubSchema);
 
