f53ecbde708132b2f2bcfa4eea049e5675ce422d
Vedant Dagadkhair <139427232+vedu111@users.noreply.github.com>
2024-04-25 18:29:51 +0530

    Update question.js

--- server/models/question.js
+++ server/models/question.js
@@ -1,15 +1,23 @@
 const mongoose = require('mongoose');
 
 const questionsubSchema = new mongoose.Schema({
-    sr_no: String, // Added sr_no field to schema
+    sr_no: String,
     type: String,
     questions: String,
     co: String,
     rbt: String,
     pi: String,
     marks: String,
-    subject: String, // Added subject field to schema
-    is_image: String
+    subject: String,
+    is_image: String,
+    image: {
+        data: Buffer,
+        contentType: String
+    },
+    Rating: {
+        type: String,
+        default: null 
+    },
 });
 
 const Question = mongoose.model('Question', questionsubSchema);
