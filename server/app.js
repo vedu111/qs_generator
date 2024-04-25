de0b445e15177cbcf7d2523de475a28418feaa22
Vedant Dagadkhair <139427232+vedu111@users.noreply.github.com>
2024-04-25 22:14:59 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const uploadRoute = require('./routes/upload');
-const fetchQuestionsRoute = require('./routes/fetchquestions'); // Import the fetchquestions route
 const path = require('path');
 const { Question, questionsubSchema } = require('./models/question'); // Import questionsubSchema
 const subInfoRouter = require('./routes/subinfo');
-const {fetchSubinfoRouter} = require('./routes/fetchsubinfo');
-const storeQbRouter = require('./routes/storeqb');
 const questionsWithImagesRouter = require('./routes/questionswithimages');
 const storeImagesRouter = require('./routes/storeimages');
-const { fetchQuestionsRouter} = require('./routes/fetchallqs');// Import');
 const quesgenRouter = require('./routes/questiongen');
-const storeImage = require('./routes/storeImage');
 const app = express();
 
 // MongoDB setup
-mongoose.connect('mongodb+srv://dagadkhairvedant:Phw40Q017YOjZzwM@quesgenerator.k2g7m6f.mongodb.net/?retryWrites=true&w=majority&appName=QUESGENERATOR', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb+srv://dagadkhairvedant:Phw40Q017YOjZzwM@quesgenerator.k2g7m6f.mongodb.net/?retryWrites=true&w=majority&appName=QUESGENERATOR', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/yourDB', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.on('error', (err) => {
@@ -33,14 +29,9 @@ app.set('view engine', 'ejs');
 
 // Routes
 app.use('/', uploadRoute);
-app.use('/', fetchQuestionsRoute); // Use the fetchquestions route
 app.use('/', subInfoRouter);
-app.use('/', fetchSubinfoRouter);
-app.use('/', storeQbRouter);
-app.use('/', storeImage);
 app.use('/', questionsWithImagesRouter);
 app.use('/', storeImagesRouter);
-app.use('/', fetchQuestionsRouter);
 app.use('/', quesgenRouter);
 
 // Serve static files from the 'public' directory
